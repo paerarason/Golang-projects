7ce5207a97924300b52e6c28c92cc9e098c6ab8d
your_github_username <paerarason1111@gmail.com>
2023-06-27 16:08:45 +0530

    heap

--- heap.go
+++ heap.go
@@ -1,11 +1,29 @@
 package main
 
-type Heap struct {
-	left  *Heap
-	right *Heap
-	data  int
+func heapify(arr *[]int, n int, i int) {
+	l := 2*i + 1
+	r := 2*i + 2
+	largest := i
+	if l < n && (*arr)[largest] < (*arr)[l] {
+		largest = l
+	} else if r < n && (*arr)[largest] < (*arr)[r] {
+		largest = r
+	}
+	if largest != i {
+		(*arr)[i], (*arr)[largest] = (*arr)[largest], (*arr)[i]
+		heapify(arr, n, largest)
+	}
+
 }
 
-func (){
+func heapsort(arr *[]int, n int) {
 
+	for i := (n / 2) - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+
+	for i := n - 1; i > 0; i-- {
+		(*arr)[0], (*arr)[i] = (*arr)[i], (*arr)[0]
+		heapify(arr, i, 0)
+	}
 }
--- main.go
+++ main.go
@@ -16,5 +16,8 @@ func main() {
 	push(&list, 35)
 	push(&list, 45)
 	printstack(&list)
+	arr := []int{78, 9, 45, 56, 14}
+	heapsort(&arr, 5)
+	fmt.Print(arr)
 
 }
