201ed0da238c91637fe668b83a75f615f1ff7631
paerarason <paerarason1111@gmail.com>
2023-12-25 21:40:49 +0530

    LCS

--- DSA/problem1.go
+++ DSA/problem1.go
@@ -87,3 +87,23 @@ func Divide(a,b float32)(float32,error){
 	}
 	return a/b, nil
 }
+
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	dp:=make([][]int,len(text1)+1)
+	for i := range dp {
+	 dp[i]= make([]int, len(text2)+1)
+ }
+	for i:=0;i<=len(text1);i++{
+		for j:=0;j<=len(text2);j++{
+			  if i==0 || j==0{
+				  dp[i][j]=0
+			  }else if text1[i-1]==text2[j-1]{
+				  dp[i][j]=dp[i-1][j-1]+1
+			  }else{
+				  dp[i][j]=max(dp[i][j-1],dp[i-1][j])
+			  }
+		}
+	}
+	return dp[len(text1)][len(text2)]
+ }
